ccf01f9629686c54389938523e782ed9faaf31a0
AlexeyMonarh <monarhweb@gmail.com>
2021-01-18 16:29:36 +0300

    Проектная-12

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const router = require('./routes/cards');
-// const bodyParser = require('body-parser');
+const router = require('./routes');
 const app = express();
 const { PORT = 3000 } = process.env;
 
-// const data = require('./data/cards.json')
-
-// app.get('/cards', (req, res) => {
-//   res.send(data);
-// })
-
-app.use(express.static('public'));
 app.use('/', router);
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-
+app.use('/', express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
-})
+});
+
+
+
+// const bodyParser = require('body-parser');
+// app.use('/', (req, res, next) => {
+//   console.log(`Request to ${req.path}`);
+//   next();
+// })
+
+// app.use(bodyParser.urlencoded({ extended: true }));
--- routes/cards.js
+++ routes/cards.js
@@ -1,20 +1,13 @@
 const router = require('express').Router();
-// const path = require('path');
-// const { cards } = require('../data/cards.json');
+const readJson = require('../utils/readJson');
+const path = require('path');
 
-// const data = require('../data/cards.json')
-
-// router.get('/cards', function (req, res) {
-
-//   res.send(data);
-// })
-
-router.get('/cards', (req, res) => {
-  res.send('Hello world')
-})
-
-// const cardData = (req, res) => {
-//   router(console.log(req))
-// }
+router.get('/', (req, res) => {
+  readJson(path.join(__dirname, '..', 'data', 'cards.json'))
+  .then((cards) => {
+    res.send(cards);
+  })
+  .catch(err => {res.status(500).send(err)})
+});
 
 module.exports = router;
