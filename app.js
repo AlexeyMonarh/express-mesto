4e3ebe0666818449906db7773778adf9ac760c72
AlexeyMonarh <monarhweb@gmail.com>
2021-01-19 17:58:41 +0300

    Проектная-12-2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./routes');
+
 const app = express();
 const { PORT = 3000 } = process.env;
 
@@ -7,5 +8,4 @@ app.use('/', express.static('public'));
 app.use('/', router);
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
 });
