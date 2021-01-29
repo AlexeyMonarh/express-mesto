25ab271b7960f7bcbea44f3478a7e6eb5b35b50f
AlexeyMonarh <monarhweb@gmail.com>
2021-01-29 20:57:56 +0300

    Проектная-13-1

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,52 @@
-const path = require('path');
-const readJson = require('../utils/readJson');
+const mongoose = require('mongoose');
+const User = require('../models/user');
 
 const getUsers = (req, res) => {
-  readJson(path.join(__dirname, '..', 'data', 'users.json'))
+  User.find({})
     .then((users) => {
-      res.send(users);
+      res.status(200).send(users);
     })
-    .catch((err) => { res.status(500).send(err); });
+    .catch(() => res.status(404).send({ message: 'Пользователи не найдены!' }));
 };
 
 const getUser = (req, res) => {
-  const { id } = req.params;
-  readJson(path.join(__dirname, '..', 'data', 'users.json'))
-    .then((users) => {
-      const user = users.find((userId) => userId._id === id);
-      if (!user) {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
-      }
-      return res.send(user);
+  User.findById(req.params._id)
+    .then((user) => {
+      res.status(200).send({ data: user });
     })
-    .catch((err) => { res.status(500).send(err); });
+    .catch((err) => {
+      if (err instanceof mongoose.CastError) {
+        return res.status(404).send({ message: 'Нет пользователя с таким id!' });
+      }
+      return res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
+    });
+};
+
+const postUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch(() => res.status(400).send({ message: 'Пользователь не добавлен! Ошибка данных' }));
+};
+
+const patchUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch(() => res.status(400).send({ message: 'Информация не обновлена! Ошибка данных' }));
+};
+
+const patchAva = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar })
+    .then((ava) => res.status(200).send({ data: ava }))
+    .catch(() => res.status(400).send({ message: 'Информация не обновлена! Ошибка данных' }));
 };
 
 module.exports = {
   getUsers,
   getUser,
+  postUser,
+  patchUser,
+  patchAva,
 };
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const controllers = require('../controllers/users');
 
 router.get('/', controllers.getUsers);
-router.get('/:id', controllers.getUser);
+router.get('/:_id', controllers.getUser);
+router.post('/', controllers.postUser);
+router.patch('/me', controllers.patchUser);
+router.patch('/me/avatar', controllers.patchAva);
 
 module.exports = router;
